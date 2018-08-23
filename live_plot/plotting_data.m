edec0d9c3e262eba593dffffc8aee832423dc1fc
minhkhang4696 <hongockhangminh@gmail.com>
2018-08-23 15:49:02 +0200

    adding figure ADLs

--- live_plot/plotting_data.m
+++ live_plot/plotting_data.m
@@ -1,7 +1,103 @@
 figure;
 subplot(2,1,1);
-plot(stick_accel_3);
-title('S5 Acceleration 1')
+plot(run_accel_1);
+title('Run Acceleration 1')
 subplot(2,1,2);
-plot(stick_gyro_3);
-title('S5 Angular Velocity 1');
+plot(run_gyro_1);
+title('Run Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(run_accel_2);
+title('Run Acceleration 2');
+subplot(2,1,2);
+plot(run_gyro_2);
+title('Run Angular Velocity 2');
+
+figure;
+subplot(2,1,1);
+plot(stair_accel_1);
+title('Clamb Stair Acceleration 1');
+subplot(2,1,2);
+plot(stair_gyro_1);
+title('Clamb Stair Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(stair_accel_2);
+title('Clamb Stair Acceleration 2');
+subplot(2,1,2);
+plot(stair_gyro_2);
+title('Clamb Stair Angular Velocity 2');
+
+figure;
+subplot(2,1,1);
+plot(step_accel_1);
+title('Step Acceleration 1');
+subplot(2,1,2);
+plot(step_gyro_1);
+title('Step Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(step_accel_2);
+title('Step Acceleration 2');
+subplot(2,1,2);
+plot(step_gyro_2);
+title('Step Angular Velocity 2');
+
+figure;
+subplot(2,1,1);
+plot(stand_accel_1);
+title('Stand up Acceleration 1');
+subplot(2,1,2);
+plot(stand_gyro_1);
+title('Stand up Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(stand_accel_2);
+title('Stand up Acceleration 2');
+subplot(2,1,2);
+plot(stand_gyro_2);
+title('Stand up Angular Velocity 2');
+
+figure;
+subplot(2,1,1);
+plot(lying_accel_1);
+title('Lying on the bed Acceleration 1');
+subplot(2,1,2);
+plot(lying_gyro_1);
+title('Lying on the bed Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(lying_accel_2);
+title('Lying on the bed Acceleration 2');
+subplot(2,1,2);
+plot(lying_gyro_2);
+title('Lying on the bed Angular Velocity 2');
+
+figure;
+subplot(2,1,1);
+plot(lying_accel_3);
+title('Lying on the bed Acceleration 3');
+subplot(2,1,2);
+plot(lying_gyro_3);
+title('Lying on the bed Angular Velocity 3');
+
+figure;
+subplot(2,1,1);
+plot(sitdown_1_accel);
+title('Sit down Acceleration 1');
+subplot(2,1,2);
+plot(sitdown_1_gyro);
+title('Sit down Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(sitdown_2_accel);
+title('Sit down Acceleration 2');
+subplot(2,1,2);
+plot(sitdown_2_gyro);
+title('Sit down Angular Velocity 2');
---
+++ live_plot/sub_plot.m
@@ -0,0 +1,15 @@
+figure;
+subplot(2,1,1);
+plot(sitdown_1_accel);
+title('Sit down Acceleration 1');
+subplot(2,1,2);
+plot(sitdown_1_gyro);
+title('Sit down Angular Velocity 1');
+
+figure;
+subplot(2,1,1);
+plot(sitdown_2_accel);
+title('Sit down Acceleration 2');
+subplot(2,1,2);
+plot(sitdown_2_gyro);
+title('Sit down Angular Velocity 2');
