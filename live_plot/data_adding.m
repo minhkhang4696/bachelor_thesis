c1de3d4b68750b97a39e2f9fdad1ce6d35d6ff8f
minhkhang4696 <hongockhangminh@gmail.com>
2018-08-30 13:57:53 +0200

    chieu 30/8

--- live_plot/data_adding.m
+++ live_plot/data_adding.m
@@ -1,8 +1,2 @@
-jumping_accel_1 = csvread('khoa_jumping_1_accel')
-jumping_gyro_1 = csvread('khoa_jumping_1_gyro')
-
-jumping_accel_2 = csvread('khoa_jumping_2_accel')
-jumping_gyro_2 = csvread('khoa_jumping_2_gyro')
-
-jumping_accel_3 = csvread('khoa_jumping_3_accel')
-jumping_gyro_3 = csvread('khoa_jumping_3_gyro')
+minh_accel_4_NOT = csvread('Minh_4T_accel_NOT')
+minh_gyro_4_NOT = csvread('Minh_4T_gyro_NOT')
